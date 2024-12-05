59afa92a07e05c2fe08cc619b762b4ce7e9b3e28
Maria Eduarda Rebaldoni <mariaeduarda2201@icloud.com>
2024-12-05 09:57:40 -0300

    Add files via upload

---
+++ phyton de hoje/aula02.py
@@ -0,0 +1,11 @@
+print('Oii! tudo bem?')
+
+nome_usuario = 'Maria Eduarda' # tipo -> str = string
+idade_usuario = 18 # tipo -> int = integer
+altura_usuario = 1.55 #tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print ('seja bem-vindo,', nome_usuario)
+print('sua idade e' , idade_usuario, 'ANOS',)
+print('sua altura e', altura_usuario, 'METROS.')
+print ('voce e maior de idade?', maior_idade)
